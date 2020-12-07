de7222ce925656129ac84f6d40905f9220a11b0c
suu-K <kswcks1457@gmail.com>
2020-12-07 18:05:34 +0900

    baekjoon

---
+++ Beakjoon/10171 고양이.cpp
@@ -0,0 +1,26 @@
+/*
+문제 : 고양이(https://www.acmicpc.net/problem/10171)
+ 예제와 같이 고양이를 출력하시오.
+
+입력
+ 없음
+
+출력
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+	return 0;
+}
---
+++ Beakjoon/10172 개.cpp
@@ -0,0 +1,28 @@
+/*
+문제 : 개(https://www.acmicpc.net/problem/10172)
+ 예제와 같이 개를 출력하시오.
+
+입력
+ 없음
+
+출력
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+	return 0;
+}
