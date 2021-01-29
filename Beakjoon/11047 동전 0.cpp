932f296f0e975b710ae825e86a8294bb13b6b329
suu-K <kswcks1457@gmail.com>
2021-01-29 12:19:35 +0900

    beakjoon

---
+++ Beakjoon/11047 동전 0.cpp
@@ -0,0 +1,34 @@
+﻿/*
+문제 : 동전 0(https://www.acmicpc.net/problem/11047)
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void init() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int main() {
+	init();
+	int n, k, tmp;
+	int sum = 0, cnt = 0;
+	vector<int> v;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+	for (int i = v.size() - 1; i >= 0 && sum != k; i--) {
+		while (sum + v[i] <= k) {
+			sum += v[i];
+			cnt++;
+		}
+	}
+	cout << cnt << endl;
+	return 0;
+}
---
+++ Beakjoon/1931 회의실 배정.cpp
@@ -0,0 +1,51 @@
+﻿/*
+문제 : 회의실 배정(https://www.acmicpc.net/problem/1931)
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void init() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+bool compare(pair<int, int> a, pair<int, int> b) {
+	if (a.second == b.second) {
+		return a.first <= b.first;
+	}
+	return a.second <= b.second;
+}
+
+int main() {
+	init();
+	int n, a, b;	//a 시작시간, b 끝나는 시간
+	int cnt = 0;
+	vector<pair<int, int>> v;
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		v.push_back(pair<int, int>(a, b));
+	}
+
+	sort(v.begin(), v.end(), compare);
+
+	int cur;
+	for (int i = 0; i < v.size();) {
+		cur = v[i].second;
+		cnt++;
+		i++;
+		while (i<v.size() && cur > v[i].first) {
+			i++;
+		}
+	}
+
+	cout << cnt << endl;
+
+	return 0;
+}
