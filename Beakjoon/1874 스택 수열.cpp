339450e9c158342c9e57084402103015b5bb18c7
suu-K <kswcks1457@gmail.com>
2021-02-04 16:22:38 +0900

    beakjoon

---
+++ Beakjoon/1874 스택 수열.cpp
@@ -0,0 +1,53 @@
+﻿/*
+문제 : 스택 수열(https://www.acmicpc.net/problem/1874)
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+void p(vector<char>& v) {
+	for (int i = 0; i < v.size(); i++) {
+		printf("%c\n", v[i]);
+	}
+}
+
+int main() {
+	int n, tmp;
+	stack<int> s;
+	vector<char> print;
+
+	int v[100000];
+	bool ok = false;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &v[i]);
+	}
+
+	for (int i = 1, j = 0; i <= n; i++) {
+		s.push(i);
+		print.push_back('+');
+
+		while (!s.empty() && s.top() == v[j]) {
+			s.pop();
+			print.push_back('-');
+			j++;
+		}
+
+		if (j == n)
+			ok = true;
+	}
+
+	if (ok)
+		p(print);
+	else
+		cout << "NO" << endl;
+
+	return 0;
+}
---
+++ Beakjoon/5086 배수와 약수.cpp
@@ -0,0 +1,31 @@
+﻿/*
+문제 : 배수와 약수(https://www.acmicpc.net/problem/5086)
+ */
+
+#include <iostream>
+
+using namespace std;
+
+void init() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int main() {
+	init();
+	int a, b;
+	cin >> a >> b;
+	while (a != 0 || b != 0) {
+		if (a > b && a % b == 0) {
+			cout << "multiple" << endl;
+		}
+		else if (a < b && b % a == 0) {
+			cout << "factor" << endl;
+		}
+		else
+			cout << "neither" << endl;
+		cin >> a >> b;
+	}
+	return 0;
+}
