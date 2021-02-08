acb17bee2cd52d1f83b6abc8c512a9e498fb84bd
suu-K <kswcks1457@gmail.com>
2021-02-08 14:22:27 +0900

    beakjoon

---
+++ Beakjoon/1037 약수.cpp
@@ -0,0 +1,33 @@
+﻿/*
+문제 : 약수(https://www.acmicpc.net/problem/1037)
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void init() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int main() {
+	init();
+	int n;
+	cin >> n;
+	vector<int> v(n, 0);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end(), greater<int>());
+	if (n % 2) {	//진짜 약수의 개수가 홀수면
+		cout << v[n / 2] * v[n / 2] << endl;
+	}
+	else {	//짝수면
+		cout << v[0] * v[n - 1] << endl;
+	}
+	return 0;
+}
